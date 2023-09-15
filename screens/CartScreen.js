6ec653a70971d7bd9448f132c2e2e79703d54610
slimane <moussaoubrahime.slimane@gmail.com>
2023-09-15 22:10:19 +0100

    finished the first part "video 1",

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -2,7 +2,7 @@ import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { decrementQty, incrementQty } from "../ProductReducer";
 import { decrementQuantity, incrementQuantity } from "../CartReducer";
 
@@ -10,74 +10,270 @@ const CartScreen = () => {
   const cart = useSelector((state) => state.cart.cart);
   const navigation = useNavigation();
   const dispatch = useDispatch();
+  const route = useRoute();
   const total = cart
     .map((item) => item.quantity * item.price)
     .reduce((curr, prev) => curr + prev, 0);
 
   return (
-    <ScrollView style={{ backgroundColor: "#f0f0f0" }}>
-      <View style={{ marginTop: 50, margin: 10 }}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Ionicons name="arrow-back-outline" size={24} color="black" />
-          <Text>Your Bucket</Text>
-        </View>
+    <>
+      <ScrollView style={{ backgroundColor: "#f0f0f0" }}>
+        {total === 0 ? (
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ marginTop: 40 }}> Your cart is Empty</Text>
+          </View>
+        ) : (
+          <View style={{ marginTop: 50, margin: 10 }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons onPress={() => navigation.goBack()}   name="arrow-back-outline" size={24} color="black" />
+              <Text>Your Bucket</Text>
+            </View>
 
-        <Pressable
-          style={{
-            backgroundColor: "white",
-            marginVertical: 10,
-            padding: 14,
-            borderRadius: 12,
-          }}
-        >
-          {cart.map((item, index) => (
-            <View
-              key={index}
+            <Pressable
               style={{
-                flexDirection: "row",
-                alignItems:"center",
-                justifyContent: "space-between",
-                margin: 12,
+                backgroundColor: "white",
+                marginVertical: 10,
+                borderRadius: 12,
               }}
             >
-              <Text style={styles.itemName}>{item.name}</Text>
-              <Pressable>
-              <View>
-                <Pressable style={styles.addContainer}>
-                  <Pressable style={styles.increaseB}>
-                    <Text
-                      onPress={() => {
-                        dispatch(decrementQty(item));
-                        dispatch(decrementQuantity(item));
-                      }}
-                      style={styles.increaseT}
-                    >
-                      -
-                    </Text>
-                  </Pressable>
+              {cart.map((item, index) => (
+                <View
+                  key={index}
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    marginVertical: 12,
+                    paddingLeft: 10,
+                  }}
+                >
+                  <Text style={styles.itemName}>{item.name}</Text>
                   <Pressable>
-                    <Text style={styles.quantity}>{item.quantity}</Text>
+                    <View>
+                      <Pressable style={styles.addContainer}>
+                        <Pressable style={styles.increaseB}>
+                          <Text
+                            onPress={() => {
+                              dispatch(decrementQty(item));
+                              dispatch(decrementQuantity(item));
+                            }}
+                            style={styles.increaseT}
+                          >
+                            -
+                          </Text>
+                        </Pressable>
+                        <Pressable>
+                          <Text style={styles.quantity}>{item.quantity}</Text>
+                        </Pressable>
+                        <Pressable
+                          onPress={() => {
+                            dispatch(incrementQty(item));
+                            dispatch(incrementQuantity(item));
+                          }}
+                          style={styles.increaseB}
+                        >
+                          <Text style={styles.increaseT}>+</Text>
+                        </Pressable>
+                      </Pressable>
+                    </View>
                   </Pressable>
-                  <Pressable
-                    onPress={() => {
-                      dispatch(incrementQty(item));
-                      dispatch(incrementQuantity(item));
-                    }}
-                    style={styles.increaseB}
-                  >
-                    <Text style={styles.increaseT}>+</Text>
-                  </Pressable>
-                </Pressable>
-              </View>
-              </Pressable>
-              <Text style={{width:80, fontSize: 16, fontWeight: "500" }}>
-                DZD {item.price * item.quantity}
+                  <Text style={{ width: 80, fontSize: 16, fontWeight: "500" }}>
+                    DZD {item.price * item.quantity}
+                  </Text>
+                </View>
+              ))}
+            </Pressable>
+            <View>
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                Billing details
               </Text>
+              <View
+                style={{
+                  backgroundColor: "white",
+                  marginVertical: 10,
+                  padding: 10,
+                  borderRadius: 12,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    Items Total
+                  </Text>
+                  <Text style={{ fontSize: 16, fontWeight: "500" }}>
+                    DZD {total}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    Dlivery Fee | 1.2KM
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#088f8f",
+                      fontSize: 16,
+                      fontWeight: "500",
+                    }}
+                  >
+                    FREE
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                    borderBottomWidth: 0.7,
+                    borderBottomColor: "gray",
+                    paddingBottom: 10,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    Free Delivery on Your order
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    Selected Date
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#088f8f",
+                      fontSize: 16,
+                      fontWeight: "500",
+                    }}
+                  >
+                    {route.params.pickUpDate[0]}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    N° Of days
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#088f8f",
+                      fontSize: 16,
+                      fontWeight: "500",
+                    }}
+                  >
+                    {route.params.no_of_days}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                    borderBottomWidth: 0.7,
+                    borderBottomColor: "gray",
+                    paddingBottom: 10,
+                  }}
+                >
+                  <Text style={{ color: "gray", fontSize: 16 }}>
+                    Selected Pick Up Time
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#088f8f",
+                      fontSize: 16,
+                      fontWeight: "500",
+                    }}
+                  >
+                    {route.params.selectedTime}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                    To Pay
+                  </Text>
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                    {" "}
+                    DZD {total + 200}
+                  </Text>
+                </View>
+              </View>
             </View>
-          ))}
+          </View>
+        )}
+      </ScrollView>
+      {total === 0 ? null : (
+        <Pressable
+          style={{
+            backgroundColor: "#088F8F",
+            padding: 10,
+            margin: 15,
+            marginTop: 0,
+            borderRadius: 7,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
+              {cart.length} items | {total} DZD
+            </Text>
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: "400",
+                color: "white",
+                marginVertical: 6,
+              }}
+            >
+              Extra charges might apply
+            </Text>
+          </View>
+
+          <Pressable onPress={() => navigation.navigate("")}>
+            <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
+              Place Order
+            </Text>
+          </Pressable>
         </Pressable>
-      </View>
-    </ScrollView>
+      )}
+    </>
   );
 };
 
@@ -91,20 +287,18 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    margin: 14,
+    // margin: 14,
   },
 
   image: { width: 70, height: 70 },
 
   itemName: {
-    width:85,
+    width: 100,
     fontSize: 16,
     fontWeight: "500",
-    // backgroundColor:"red",
   },
 
   itemPrice: {
-    width: 80,
     fontSize: 15,
     color: "gray",
   },
@@ -113,7 +307,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    width:100,
   },
 
   increaseB: {
@@ -139,7 +332,6 @@ const styles = StyleSheet.create({
     color: "#088F8F",
     paddingHorizontal: 8,
     fontWeight: "600",
-
   },
 
   addButton: {
--- screens/PickUpscreen.js
+++ screens/PickUpscreen.js
@@ -13,16 +13,18 @@ import HorizontalDatepicker from "@awrminkhodaei/react-native-horizontal-datepic
 import { useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import CartScreen from "./CartScreen";
+import moment from "moment-jalaali";
 
 const PickUpscreen = () => {
   const cart = useSelector((state) => state.cart.cart);
-  
+
   const total = cart
     .map((item) => item.quantity * item.price)
     .reduce((curr, prev) => curr + prev, 0);
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedTime, setSelectedTime] = useState([]);
-  const [delivry, setDelivery] = useState([]);
+  const [selectedDate, setSelectedDate] = useState([]);
+  const [selectedTime, setSelectedTime] = useState("");
+  const [delivry, setDelivery] = useState('');
+  // console.log(selectedDate,endDate);
   const delivryTime = [
     {
       id: "0",
@@ -53,7 +55,7 @@ const PickUpscreen = () => {
     },
     {
       id: "1",
-      time: "11:00 PM",
+      time: "01:00 PM",
     },
     {
       id: "2",
@@ -74,163 +76,178 @@ const PickUpscreen = () => {
   ];
   const navigation = useNavigation();
   const proceedToCart = () => {
-    if(!selectedDate || !selectedTime || !delivry){
+    console.log(selectedDate )
+    if (selectedDate.length==0 || !selectedTime || !delivry) {
       const createTwoButtonAlert = () =>
-      Alert.alert('Empty Or Invalid', 'Please select all the fields', [
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel',
-        },
-        {text: 'OK', onPress: () => console.log('OK Pressed')},
-      ]);
-      createTwoButtonAlert()
+        Alert.alert("Empty Or Invalid", "Please select all the fields", [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ]);
+      createTwoButtonAlert();
     }
 
-    if(selectedDate && selectedTime && delivry){
-      navigation.replace("Cart")
+    if (selectedDate.length>2 && selectedTime && delivry) {
+      navigation.replace("Cart", {
+        pickUpDate: [selectedDate],
+        selectedTime: selectedTime,
+        no_of_days: delivry,
+      });
     }
-  }
+  };
+  let startDate = moment(new Date());
+  let endDate = startDate.add(1, "week");
+  
   return (
     <>
-    <SafeAreaView>
-      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
-        Enter Address
-      </Text>
-      <TextInput
-        style={{
-          padding: 40,
-          borderColor: "gray",
-          borderWidth: 0.7,
-          paddingVertical: 80,
-          borderRadius: 9,
-          margin: 10,
-        }}
-      />
-      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
-        Pick Up Date
-      </Text>
-      <HorizontalDatepicker
-        mode="gregorian"
-        startDate={new Date("2023-09-12")}
-        endDate={new Date("2023-09-22")}
-        initialSelectedDate={new Date("2020-08-22")}
-        onSelectedDateChange={(date) => setSelectedDate(date)}
-        selectedItemWidth={170}
-        unselectedItemWidth={38}
-        itemHeight={38}
-        itemRadius={10}
-        selectedItemTextStyle={styles.selectedItemTextStyle}
-        unselectedItemTextStyle={styles.selectedItemTextStyle}
-        selectedItemBackgroundColor="#222831"
-        unselectedItemBackgroundColor="#ececec"
-        flatListContainerStyle={styles.flatListContainerStyle}
-      />
+      <SafeAreaView>
+        <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+          Enter Address
+        </Text>
+        <TextInput
+          style={{
+            padding: 40,
+            borderColor: "gray",
+            borderWidth: 0.7,
+            paddingVertical: 80,
+            borderRadius: 9,
+            margin: 10,
+          }}
+        />
+        <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+          Pick Up Date
+        </Text>
+        <HorizontalDatepicker
+          mode="gregorian"
+          startDate={new Date()}
+          endDate={endDate}
+          initialSelectedDate={new Date("2020-08-22")}
+          onSelectedDateChange={(date) => setSelectedDate(date.toLocaleDateString())}
+          selectedItemWidth={170}
+          unselectedItemWidth={38}
+          itemHeight={38}
+          itemRadius={10}
+          selectedItemTextStyle={styles.selectedItemTextStyle}
+          unselectedItemTextStyle={styles.selectedItemTextStyle}
+          selectedItemBackgroundColor="#222831"
+          unselectedItemBackgroundColor="#ececec"
+          flatListContainerStyle={styles.flatListContainerStyle}
+        />
 
-      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
-        Select Time
-      </Text>
+        <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+          Select Time
+        </Text>
 
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {times.map((item, index) => (
-          <Pressable
-            key={index}
-            onPress={() => selectedTime === item.id? setSelectedTime([]) : setSelectedTime(item.id) }
-            style={
-              selectedTime.includes(item.id)
-                ? {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 15,
-                    borderColor: "red",
-                    borderWidth: .7,
-                    paddingHorizontal:50,
-                  }
-                : {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 15,
-                    borderColor: "gray",
-                    borderWidth: .7,
-                  }
-            }
-          >
-            <Text>{item.time}</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {times.map((item, index) => (
+            <Pressable
+              key={index}
+              onPress={() =>
+                selectedTime === item.id
+                  ? setSelectedTime([])
+                  : setSelectedTime(item.time)
+              }
+              style={
+                selectedTime.includes(item.time)
+                  ? {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 15,
+                      borderColor: "red",
+                      borderWidth: 0.7,
+                      paddingHorizontal: 50,
+                    }
+                  : {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 15,
+                      borderColor: "gray",
+                      borderWidth: 0.7,
+                    }
+              }
+            >
+              <Text>{item.time}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+
+        <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
+          Delivery Date
+        </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {delivryTime.map((item, i) => (
+            <Pressable
+              key={i}
+              onPress={() =>
+                delivry === item.name ? setDelivery([]) : setDelivery(item.name)
+              }
+              // onPress={() => setDelivery(item.name)}
+              style={
+                delivry.includes(item.name)
+                  ? {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 15,
+                      borderColor: "red",
+                      borderWidth: 0.7,
+                      paddingHorizontal: 50,
+                    }
+                  : {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 15,
+                      borderColor: "gray",
+                      borderWidth: 0.7,
+                    }
+              }
+            >
+              <Text>{item.name}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+
+      {total === 0 ? null : (
+        <Pressable
+          style={{
+            backgroundColor: "#088F8F",
+            padding: 10,
+            marginTop: "auto",
+            marginBottom: 40,
+            margin: 15,
+            borderRadius: 7,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
+              {cart.length} items | {total} DZD
+            </Text>
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: "400",
+                color: "white",
+                marginVertical: 6,
+              }}
+            >
+              Extra charges might apply
+            </Text>
+          </View>
+
+          <Pressable onPress={proceedToCart}>
+            <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
+              Preceed to Cart
+            </Text>
           </Pressable>
-        ))}
-      </ScrollView>
-
-      <Text style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
-      Delivery Date
-      </Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {delivryTime.map((item, i) => (
-          <Pressable
-            key={i}
-            onPress={() => delivry===item.name ? setDelivery([]): setDelivery(item.name) }
-            // onPress={() => setDelivery(item.name)}
-            style={delivry.includes(item.name) ? {
-              margin: 10,
-              borderRadius: 7,
-              padding: 15,
-              borderColor: "red",
-              borderWidth: .7,
-              paddingHorizontal:50,         
-              
-            }
-          : {
-              margin: 10,
-              borderRadius: 7,
-              padding: 15,
-              borderColor: "gray",
-              borderWidth: 0.7,
-            }}
-          >
-            <Text>{item.name}</Text>
-          </Pressable>
-        ))}
-      </ScrollView>
-    </SafeAreaView>
-
-    
-     {total === 0 ? null : (
-      <Pressable
-        style={{
-          backgroundColor: "#088F8F",
-          padding: 10,
-          marginTop:"auto",
-          marginBottom: 40,
-          margin: 15,
-          borderRadius: 7,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <View>
-          <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
-            {cart.length} items | {total} DZD
-          </Text>
-          <Text
-            style={{
-              fontSize: 13,
-              fontWeight: "400",
-              color: "white",
-              marginVertical: 6,
-            }}
-          >
-            Extra charges might apply
-          </Text>
-        </View>
-
-        <Pressable onPress={proceedToCart} >
-          <Text style={{ fontSize: 17, fontWeight: "600", color: "white" }}>
-            Preceed to Cart
-          </Text>
         </Pressable>
-      </Pressable>
-    )}
-  </>
+      )}
+    </>
   );
 };
 
